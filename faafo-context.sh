892a757e83213dd537c5b192abc12f574cf8cfec
Enol Fernández <enol.fernandez@egi.eu>
2015-06-23 10:37:44 +0100

    Update faafo-context.sh

--- faafo-context.sh
+++ faafo-context.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+apt-get -y install curl
 curl -L -s https://git.openstack.org/cgit/stackforge/faafo/plain/contrib/install.sh | bash -s -- \
         -i faafo -i messaging -r api -r worker -r demo
